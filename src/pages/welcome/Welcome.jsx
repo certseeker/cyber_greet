f774cf29d78aeb31801b25d0bd02db10f289574a
CertSeeker <133781991+certseeker@users.noreply.github.com>
2023-09-20 04:45:22 -0400

    fourth commit: app styling

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from "../welcome/Welcome.css"
 
 
 export default function Welcome() {
   return (
     <div className='overall-page'>
-      <h1>Welcome</h1>
+      <h1 className='page-name'>Cyber Greet</h1>
+      <div>
+        <p className='description'>
+          Your one stop for your dream cyber security career upgrade. 
+        </p>
+      </div>
       <div className='button-box'>
         <Link to="/signup" className='button'>Sign Up</Link>
         <Link to="/login" className='button'>Log In</Link>
